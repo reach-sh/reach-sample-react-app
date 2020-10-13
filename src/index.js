47bff42a81a278d1041871528ce3e2756b1bc33c
Dan Burton <danburton.email@gmail.com>
2020-10-13 18:03:27 -0400

    App and Alice updated

--- src/index.js
+++ src/index.js
@@ -20,258 +20,336 @@ function renderDOM() {
 class App extends React.Component {
   constructor(props) {
     super(props);
+    // Modes:
+    // * ConnectAccount: connectAccount
+    // * FundAccount: fundAccount or skipFundAccount
+    // * SelectRole: selectRole
+    // * RunRole: defined in Alice & Bob sub-components
     this.state = {
-      isBob: false,
+      mode: 'ConnectAccount',
+      // Set by ConnectAccount
+      acc: undefined,
+      addr: undefined,
+      bal: undefined,
+      // Set by FundAccount
+      fundAmountStandard: undefined,
+      // Set by SelectRole
+      role: undefined,
     }
   }
 
-  swapRoles() {
-    this.setState({isAlice: !this.state.isAlice});
+  componentDidMount() {
+    if (this.state.mode === 'ConnectAccount') {
+      this.connectAccount();
+    }
+  }
+
+  async connectAccount() {
+    const acc = await reach.getDefaultAccount();
+    const addr = await acc.networkAccount.getAddress();
+    const balAtomic = await reach.balanceOf(acc);
+    const bal = reach.formatCurrency(balAtomic, 4);
+    try {
+      const faucet = await reach.getFaucet();
+      this.setState({mode: 'FundAccount', acc, addr, bal, faucet});
+    } catch (e) {
+      // Couldn't find a faucet; skip to SelectRole
+      this.setState({mode: 'SelectRole', acc, addr, bal});
+    }
+  }
+
+  async fundAmountChanged(fundAmountStandard) {
+    this.setState({fundAmountStandard});
+  }
+
+  async fundAccount() {
+    const {faucet, acc, fundAmountStandard = '10'} = this.state;
+    const amountAtomic = reach.parseCurrency(fundAmountStandard);
+    reach.transfer(faucet, acc, amountAtomic);
+    this.setState({mode: 'SelectRole'});
+  }
+
+  async skipFundAccount() {
+    this.setState({mode: 'SelectRole'});
+  }
+
+  async selectRole(role) {
+    this.setState({mode: 'RunRole', role});
   }
 
   render() {
+    let app = null;
+    switch (this.state.mode) {
+      case 'ConnectAccount':
+        app = (
+          <div>
+            Please wait while we connect to your account.
+            If this takes more than a few seconds, there may be something wrong.
+          </div>
+        );
+        break;
+      case 'FundAccount':
+        app = (
+          <div>
+            <h1>Fund account</h1>
+            <br />
+            Address: {this.state.addr}
+            <br />
+            Balance: {this.state.bal} {reach.standardUnit}
+            <hr />
+            Would you like to fund your account with additional {reach.standardUnit}?
+            <br />
+            (This only works on certain devnets)
+            <br />
+            <input
+              type='number'
+              onChange={(e) => {
+                this.fundAmountChanged(e.currentTarget.value);
+              }}
+            />
+            <button onClick={() => this.fundAccount()}>Fund Account</button>
+            <button onClick={() => this.skipFundAccount()}>Skip</button>
+          </div>
+        );
+        break;
+      case 'SelectRole':
+        app = (
+          <div>
+            Please select a role:
+            <br />
+            <button
+              onClick={() => this.selectRole(<Alice acc={this.state.acc} />)}
+            >Alice</button>
+            <button
+              onClick={() => this.selectRole(<Bob acc={this.state.acc} />)}
+            >Bob</button>
+            <p>
+              <strong>Alice</strong>:
+              <br /> Requests payment from Bob in order to reveal a secret.
+            </p>
+            <p>
+              <strong>Bob</strong>:
+              <br /> Pays Alice in order for her to reveal a secret.
+            </p>
+          </div>
+        );
+        break;
+      case 'RunRole':
+        app = this.state.role;
+        break;
+      default:
+        app = (
+          <div>
+            Sorry, something went wrong.
+            Please refresh the page.
+          </div>
+        );
+        break;
+    }
+
     return (
       <div className="App">
         <header className="App-header">
-          <FundMe />
-          <button
-            style={{fontSize: '32px'}}
-            onClick={() => this.setState({isBob: !this.state.isBob})}>
-            Swap roles
-          </button>
-
-          {this.state.isBob ? <Bob /> : <Alice />}
+          {app}
         </header>
       </div>
     );
   }
 }
 
-class FundMe extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      acc: undefined,
-      bal: undefined,
-      funding: false,
-    }
-  }
 
-  async componentDidMount() {
-    const acc = await reach.getDefaultAccount();
-    this.setState({acc});
-  }
-
-  componentDidUpdate(oldProps, oldState) {
-    if (this.state.acc !== oldState.acc) {
-      this.refreshBal();
-    }
-  }
-
-  async refreshBal() {
-    const bal = await reach.balanceOf(this.state.acc);
-    this.setState({bal});
-  }
-
-  emptyBalHuh() {
-    const {acc, bal} = this.state;
-    if (!bal) return false;
-    console.log(bal.toString());
-    return acc && bal && !bal.gt(0);
-  }
-
-  async fundMe() {
-    const {acc} = this.state;
-    this.setState({funding: true});
-    const faucet = await reach.getFaucet();
-    await reach.transfer(faucet, acc, reach.parseCurrency('100'));
-    await sleep(1000); // XXX
-    this.refreshBal();
-  }
-
-  render() {
-    if (this.emptyBalHuh()) {
-      return (
-        <div>
-          I see your account is empty.
-          If you are on a devnet, you can receive funds from the faucet.
-          <br />
-          <button
-            style={{fontSize: '24px'}}
-            onClick={() => this.fundMe()}
-            disabled={this.state.funding}
-          >Fund me</button>
-        </div>
-      )
-    } else {
-      return '';
-    }
-  }
-}
-
-export default FundMe
-
-
-const defaultAmt = 5; // in the standard unit
+const defaultInfo = 'the cake is a lie';
+const defaultRequestStandard = '0.5';
 
 class Alice extends React.Component {
   constructor(props) {
     super(props);
+    // Modes:
+    // * Deploy: deploy
+    // * EnterInfo: enterInfo
+    // * EnterRequest: enterRequest
+    // * RunBackend: runBackend
+    // * BackendRunning: backendRan
+    // * BackendRan
     this.state = {
-      acc: undefined,
+      mode: 'Deploy',
+      acc: props.acc,
+      // Set by Deploy
       ctc: undefined,
-      deployed: undefined,
-      deploying: undefined,
-      running: undefined,
-      ran: undefined,
+      // Set (async) by Deploy
+      ctcInfoStr: undefined,
+      // Set by EnterSecret
       secret: undefined,
-      amt: reach.parseCurrency(defaultAmt),
+      // Set by EnterAmount
+      requestStandard: undefined,
     };
-  }
-
-  async componentDidMount() {
-    const acc = await reach.getDefaultAccount();
-    this.setState({acc});
   }
 
   async deploy() {
-    this.setState({deploying: true, running: false, ran: false});
     const {acc} = this.state;
-    const ctc = await acc.deploy(backend);
-    this.setState({ctc});
-    await ctc.getInfo();
-    this.setState({deploying: false, deployed: true});
+    const ctc = acc.deploy(backend);
+    this.setState({mode: 'EnterInfo', ctc});
+
+    // (async portion: may be delayed)
+    const ctcInfoStr = JSON.stringify(await ctc.getInfo(), null, 2);
+    this.setState({ctcInfoStr});
   }
 
-  async run() {
-    const {ctc} = this.state;
-    // XXX get Info from a text box or something
-    const interact = {
-      request: this.state.amt,
-      info: this.state.secret,
-    };
-    this.setState({running: true, ran: false});
+  infoChanged(info) {
+    this.setState({info});
+  }
+
+  enterInfo() {
+    let {info} = this.state;
+    info = info || defaultInfo;
+    this.setState({mode: 'EnterRequest', info});
+  }
+
+  requestChanged(requestStandard) {
+    this.setState({requestStandard});
+  }
+
+  enterRequest() {
+    let {requestStandard} = this.state;
+    requestStandard = requestStandard || defaultRequestStandard;
+    this.setState({mode: 'RunBackend', requestStandard});
+  }
+
+  async runBackend() {
+    const {ctc, requestStandard, info} = this.state;
+    this.setState({mode: 'BackendRunning'});
+
+    // (async portion: may be delayed)
+    const request = reach.parseCurrency(requestStandard);
+    const interact = {request, info};
     await backend.Alice(reach, ctc, interact);
-    this.setState({running: false, ran: true});
+    this.setState({mode: 'BackendRan'});
   }
 
-  amountChanged(value) {
-    if (!value && value !== 0) {
-      this.setState({amt: undefined});
-      return;
-    }
-    const amt = reach.parseCurrency(value);
-    this.setState({amt}); // amt is BigNumber of WEI
-  }
-
-  textareaChanged(secret) {
-    this.setState({secret});
+  async copyToClipborad(button) {
+    navigator.clipboard.writeText(this.state.ctcInfoStr);
+    const origInnerHTML = button.innerHTML;
+    button.innerHTML = 'Copied!';
+    button.disabled = true;
+    await sleep(1000);
+    button.innerHTML = origInnerHTML;
+    button.disabled = false;
   }
 
   render() {
+    let alice = null;
+    switch (this.state.mode) {
+      case 'Deploy':
+        alice = (
+          <div>
+            As Alice, it is your job to deploy the contract.
+            <br />
+            <button
+              onClick={() => this.deploy()}
+            >Deploy</button>
+          </div>
+        );
+        break;
+      case 'EnterInfo':
+        alice = (
+          <div>
+            Alice, what is your secret info?
+            <br />
+            <textarea
+              onChange={(e) => this.infoChanged(e.currentTarget.value)}
+              placeholder={defaultInfo}
+            />
+            <br />
+            <button onClick={() => this.enterInfo()}
+            >Submit secret info</button>
+          </div>
+        )
+        break;
+      case 'EnterRequest':
+        alice = (
+          <div>
+            Alice, how much {reach.standardUnit} should Bob pay you
+            to reveal this info?
+            <br />
+            <input
+              type='number'
+              onChange={(e) => this.requestChanged(e.currentTarget.value)}
+              placeholder={defaultRequestStandard}
+            />
+            <br />
+            <button onClick={() => this.enterRequest()}
+            >Submit request</button>
+          </div>
+        );
+        break;
+      case 'RunBackend':
+        alice = (
+          <div>
+            <p>
+              You request <strong>{this.state.requestStandard}</strong> {reach.standardUnit + ' '}
+              to reveal secret info: <strong>{this.state.info}</strong>
+            </p>
+            <p>
+              Ready to connect to the contract?
+            </p>
+            <p>
+              You will be prompted to pay for two transactions.
+              The first transaction will publish your requested amount,
+              and the second will publish your secret while simultaneously
+              retrieving the requested amount from the contract.
+            </p>
+            <button
+              onClick={() => this.runBackend()}
+            >Connect</button>
+          </div>
+        );
+        break;
+      case 'BackendRunning':
+        if (this.state.ctcInfoStr === undefined) {
+          alice = (
+            <div>
+              Waiting for the contract to deploy...
+              If this takes more than 1 min, something may be wrong.
+            </div>
+          )
+        } else {
+          alice = (
+            <div>
+              <h2>Contract Info</h2>
+              The contract is running!
+              Please give Bob the following contract info.
+
+              <pre>
+                {this.state.ctcInfoStr}
+              </pre>
+              <br />
+              <button
+                onClick={async (e) => this.copyToClipborad(e.currentTarget)}
+              >Copy to clipboard</button>
+              <br />
+
+              You will be automatically prompted to approve the next transaction
+              once Bob has paid the requested amount into the contract.
+            </div>
+          );
+        }
+        break;
+      case 'BackendRan':
+        alice = (
+          <div>
+            Thank you, Alice.
+            The contract has run to completion.
+          </div>
+        )
+        break;
+      default:
+        alice = (
+          <div>Sorry, Alice, something went wrong.</div>
+        );
+    }
     return (
       <div className="Alice">
-        Current role: <strong>Alice</strong>.
-        <br /> Requests payment from Bob in order to reveal a secret.
-        <br /> Alice, what is your secret?
-        <br />
-        <textarea
-          style={{fontSize: '24px'}}
-          onChange={(e) => this.textareaChanged(e.currentTarget.value)}
-        />
-        <br />
-        How much {reach.standardUnit} do you want in exchange for revealing this secret?
-        <br />
-        <input
-          style={{fontSize: '24px'}}
-          onChange={(e) => this.amountChanged(e.currentTarget.value)}
-          type='number'
-          defaultValue={defaultAmt}
-        />
-        <br />
-        <br />
-        <button
-          disabled={
-            !this.state.acc
-            || this.state.deployed || this.state.deploying
-          }
-          style={{fontSize: '32px'}}
-          onClick={() => this.deploy()}
-        >Deploy</button>
-        <button
-          disabled={
-            !this.state.acc
-            || this.state.ran || this.state.running
-            || !(this.state.deployed || this.state.deploying)
-            || !this.state.secret || !this.state.amt
-          }
-          style={{fontSize: '32px'}}
-          onClick={() => this.run()}
-        >Run</button>
-        <br />
-        App state:
-        {!(this.state.deployed || this.state.deploying) ? ' Waiting to deploy...' : ''}
-        {this.state.deploying? ' Deploying...' : ''}
-        {(! this.state.ran && this.state.deployed && !this.state.running) ? ' Waiting to run' : ''}
-        {this.state.running ? ' Running...' : ''}
-        {this.state.ran ? ' Completed' : ''}
-        <CtcInfo ref={this.ctcInfoRef} ctc={this.state.ctc} />
-      </div>
-    );
-  }
-}
-
-const defaultCopyButtonText = 'Copy to clipboard';
-
-class CtcInfo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ctcInfoStr: 'no contract deployed',
-      copyButtonText: defaultCopyButtonText,
-      copyButtonDisabled: true,
-    }
-  }
-
-  componentDidMount() {
-    this.ctcDidUpdate();
-  }
-
-  componentDidUpdate(prevProps) {
-    if ((this.props.ctc !== prevProps.ctc)) {
-      this.ctcDidUpdate();
-    }
-  }
-
-  async ctcDidUpdate() {
-    const {ctc} = this.props;
-    if (ctc) {
-      const ctcInfoStr = 'deploying...';
-      this.setState({ctcInfoStr});
-      const ctcInfo = await ctc.getInfo()
-      this.updateInfo(ctcInfo);
-    }
-  }
-
-  updateInfo(ctcInfo) {
-    const ctcInfoStr = JSON.stringify(ctcInfo, null, 2);
-    this.setState({ctcInfoStr, copyButtonDisabled: false});
-  }
-
-  async copyCtcInfo() {
-    navigator.clipboard.writeText(this.state.ctcInfoStr);
-    this.setState({copyButtonText: 'Contract info copied!', copyButtonDisabled: true});
-    await sleep(2000);
-    this.setState({copyButtonText: defaultCopyButtonText, copyButtonDisabled: false})
-  }
-
-  render() {
-    return (
-      <div>
-        Contract Info: <button disabled={this.state.copyButtonDisabled} onClick={() => this.copyCtcInfo()}>{this.state.copyButtonText}</button>
-        <pre style={{fontSize: '12px', textAlign: 'left', border: '1px solid', padding: '5px'}}>
-          {this.state.ctcInfoStr}
-        </pre>
+        {alice}
       </div>
     );
   }
